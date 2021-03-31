064858268aadb2d3aa8841a7eea8c0c2957b4a8b
titanpolo <tera.thalespolo@gmail.com>
2021-03-31 01:17:03 -0300

    Commit after "breeze:install" command (ep19)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\{
-    Controller,
-    PostController
-};
-use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,36 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// pesquisar uso do patch
-// Route::patch('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Search
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-
-// Index to menu
-Route::get('/', [Controller::class, 'menu'])->name('menu');
-
-// Index to welcome
-Route::get('/welcome', [Controller::class, 'welcome'])->name('welcome');
-
-// Index to read all registers
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-
-// Index to create
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-// Create
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
-// Index to read one register
-Route::get('/posts/show/{id}', [PostController::class, 'show'])->name('posts.show');
-
-// Delete
-Route::delete('/posts/destroy/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-
-// Index to update
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-
-// Update
-Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
+require __DIR__.'/auth.php';
