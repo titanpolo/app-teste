5c71f0e6f94eca7ad31ae12c5eb1df39a7614db8
titanpolo <tera.thalespolo@gmail.com>
2021-04-01 11:46:13 -0300

    install dependencies "npm install && npm run dev"

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
 
 // Index to menu
-// Route::get('/', [Controller::class, 'menu'])->name('menu');
+Route::get('/home', [Controller::class, 'menu'])->name('menu');
 
 // Index to welcome
 Route::get('/', [Controller::class, 'welcome'])->name('welcome');
